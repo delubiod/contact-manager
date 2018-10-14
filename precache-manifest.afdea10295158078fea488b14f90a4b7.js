ecbdf2ff185a8b10d505fb6531f64d68c4345c89
delubio lopes de paula <delubiod@gmail.com>
2018-10-13 22:49:04 -0300

    Updates

---
+++ precache-manifest.afdea10295158078fea488b14f90a4b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b8fdbf7579a38197298",
+    "url": "/contact-manager/static/js/runtime~main.2b8fdbf7.js"
+  },
+  {
+    "revision": "6cd854838f9873d91c29",
+    "url": "/contact-manager/static/js/main.6cd85483.chunk.js"
+  },
+  {
+    "revision": "1c2272722bc69f150eb2",
+    "url": "/contact-manager/static/js/1.1c227272.chunk.js"
+  },
+  {
+    "revision": "6cd854838f9873d91c29",
+    "url": "/contact-manager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "1c2272722bc69f150eb2",
+    "url": "/contact-manager/static/css/1.21241b92.chunk.css"
+  },
+  {
+    "revision": "3787740e6d4ab6b2c4fdfb6abf108805",
+    "url": "/contact-manager/index.html"
+  }
+];
